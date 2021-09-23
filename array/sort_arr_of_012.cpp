3e2ca328f9c4771ce56eb6b88cbacae7be34e75a
sejal <sejalzambare7@gmail.com>
2021-09-23 21:49:52 +0530

    add sorting_012

---
+++ array/sort_arr_of_012.cpp
@@ -0,0 +1,56 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+    public:
+    void sort012(int a[], int n)
+    {
+        // coode here
+        int count[4];
+        for(int i = 0 ; i<4;i++){
+            count[i] = 0;
+        }
+        for(int i = 0; i<n ; i++){
+            count[a[i]]++;
+        }
+        int k = 0;
+        for(int i = 0 ;i<4;i++){
+            for(int j = 0 ; j<count[i];j++){
+                a[k] = i;
+                k++;
+            }
+        }
+        
+        
+        
+    }
+    
+};
+
+
+int main(){
+    int t;
+    cin >> t;
+
+    while(t--){
+        int n;
+        cin >>n;
+        int a[n];
+        for(int i=0;i<n;i++){
+            cin >> a[i];
+        }
+
+        Solution ob;
+        ob.sort012(a, n);
+
+        for(int i=0;i<n;i++){
+            cout << a[i]  << " ";
+        }
+
+        cout << endl;
+        
+        
+    }
+    return 0;
+}
